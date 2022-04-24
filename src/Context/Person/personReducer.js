14bd16686511bff720a9947fa0dcdbeafc4957e9
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-24 14:36:05 -0500

    Agregamos la accion de crear un tipo de persona

--- src/Context/Person/personReducer.js
+++ src/Context/Person/personReducer.js
@@ -2,6 +2,20 @@ import { types_person } from "../../Types/types.person";
 
 export const personReducer = ( state, action ) => {
     switch( action.type ) {
+        case types_person.addPerson:
+            return {
+                ...state,
+                peopleList: [ ...state.peopleList, action.payload ],
+                message: action.payload.message,
+                typeMessage: 'alert-ok',
+            }
+        case types_person.addPersonFailed:
+            return {
+                ...state,
+                error: true,
+                message: action.payload,
+                typeMessage: 'alert-error',
+            }
         case types_person.removeMessages: 
             return {
                 ...state,
--- src/Context/Person/personState.js
+++ src/Context/Person/personState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import { clientAxios } from '../../Config/Axios';
 import { types_person } from '../../Types/types.person';
 
 import { PersonContext } from './PersonContext';
@@ -24,6 +25,30 @@ export const PersonState = ( props ) => {
         }, 3000 );
     }
 
+    const createPerson = async ( data, type = '' ) => { 
+        try {
+            const response = await clientAxios.post( `/api/${ type }`, {
+                name: data.name,
+                lastName: data.lastName,
+                rfc: data.rfc,
+                address: data.address,
+                email: data.email,
+                phoneNumber: data.phoneNumber
+            });
+            dispatch({
+                type: types_person.addPerson,
+                payload: response.data
+            });
+        } catch (error) {
+            dispatch({
+                type: types_person.addPersonFailed,
+                payload: error.response.data.errors.Description[0]
+            });
+        }
+
+        deleteMessage();
+    }
+
     return (
         <PersonContext.Provider
             value={{
@@ -35,6 +60,8 @@ export const PersonState = ( props ) => {
                 searchModeStatus: state.searchModeStatus,
                 statusEditModePerson: state.statusEditModePerson,
                 infPersonEdit: state.infPersonEdit,
+                createPerson,
+                deleteMessage,
             }}
         >
             { props.children }

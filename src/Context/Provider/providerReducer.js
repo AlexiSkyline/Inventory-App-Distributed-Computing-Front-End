c50c4b961ef0ca3357e509df90572111f538696f
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-25 20:25:43 -0500

    Agregamos la acción de crear un nuevo Proveedor

--- src/Context/Provider/providerReducer.js
+++ src/Context/Provider/providerReducer.js
@@ -1,6 +1,28 @@
+import { types_provider } from "../../Types/types.provider";
 
 export const providerReducer = ( state, action ) => {
     switch( action.type ) { 
+        case types_provider.addProvider:
+            return {
+                ...state,
+                providerList: [ action.payload, ...state.providerList ],
+                message: action.payload.message,
+                typeMessage: 'alert-ok'
+            }
+        case types_provider.addProviderFailed:
+            return {
+                ...state,
+                error: true,
+                message: action.payload,
+                typeMessage: 'alert-error'
+            }
+        case types_provider.removeMessages:
+            return {
+                ...state,
+                message: '',
+                typeMessage: '',
+                error: false
+            }
         default:
             return state;
     }
--- src/Context/Provider/providerState.js
+++ src/Context/Provider/providerState.js
@@ -1,8 +1,11 @@
 import React, { useReducer } from 'react';
+import { clientAxios } from '../../Config/Axios';
+import { types_provider } from '../../Types/types.provider';
 import { ProviderContext } from './ProviderContext';
 import { providerReducer } from './providerReducer';
 
 export const providerState = ( props ) => {
+    const path = '/api/Provider';
     const initialState = {
         providerList: [],
         error: null,
@@ -16,6 +19,36 @@ export const providerState = ( props ) => {
 
     const [ state, dispatch ] = useReducer( providerReducer, initialState );
 
+    const deleteMessage = () => { 
+        setTimeout(() => {
+            dispatch({ type: types_provider.removeMessages });
+        }, 3000 );
+    }
+
+    const createProvider = async ( data ) => {
+        try {
+            const response = await clientAxios.post( path, {
+                name: data.name,
+                lastName: data.lastName,
+                rfc: data.rfc,
+                address: data.address,
+                email: data.email,
+                phoneNumber: data.phoneNumber
+            });
+            dispatch({
+                type: types_provider.addProvider,
+                payload: response.data
+            });
+        } catch (error) {
+            dispatch({
+                type: types_provider.addProviderFailed,
+                payload: error.response.data.errors.Description[0]
+            });
+        }
+
+        deleteMessage();
+    }
+
     return (
         <ProviderContext.Provider
             value={{
@@ -26,6 +59,7 @@ export const providerState = ( props ) => {
                 searchModeStatus: state.searchModeStatus,
                 statusEditModeProvider: state.statusEditModeProvider,
                 infProviderEdit: state.infProviderEdit,
+                deleteMessage,
             }}
         >
             {props.children}

c2261c23c3e6db83230635114a0ddffd545e4e59
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-26 15:28:21 -0500

    Agregamos la acción de Crear un vendedor

--- src/Context/Seller/sellerReducer.js
+++ src/Context/Seller/sellerReducer.js
@@ -1,5 +1,21 @@
+import { types_seller } from '../../Types/types.seller';
+
 export const sellerReducer = ( state, action ) => {
     switch( action.type ) {
+        case types_seller.addSeller:
+            return {
+                ...state,
+                sellerList: [ ...state.sellerList, action.payload ],
+                message: action.payload.message,
+                typeMessage: 'alert-ok'
+            }
+        case types_seller.removeMessages: 
+            return {
+                ...state,
+                message: '',
+                typeMessage: '',
+                error: false,
+            }
         default:
             return state;
     }
--- src/Context/Seller/sellerState.js
+++ src/Context/Seller/sellerState.js
@@ -1,8 +1,11 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
+import { clientAxios } from '../../Config/Axios';
+import { types_seller } from '../../Types/types.seller';
 import { SellerContext } from './SellerContext';
 import { sellerReducer } from './sellerReducer';
 
 export const SellerState = ( props ) => {
+    const path = '/api/Seller';
     const initialState = {
         sellerList: [],
         error: null,
@@ -13,7 +16,40 @@ export const SellerState = ( props ) => {
         statusEditModeSeller: false,
         infSellerEdit: null,
     }
-    const [state, dispatch] = useReducer(sellerReducer, initialState);
+
+    const [ state, dispatch ] = useReducer( sellerReducer, initialState );
+
+    const deleteMessage = () => { 
+        setTimeout(() => {
+            dispatch({ type: types_seller.removeMessages });
+        }, 3000 );
+    }
+
+    const createSeller = async ( data ) => {
+        try {
+            const response = await clientAxios.post( path, {
+                name: data.name,
+                lastName: data.lastName,
+                rfc: data.rfc,
+                address: data.address,
+                email: data.email,
+                phoneNumber: data.phoneNumber,
+                userName: data.userName,
+                password: data.password
+            });
+            dispatch({
+                type: types_seller.addSeller,
+                payload: response.data
+            });
+        } catch (error) {
+            dispatch({
+                type: types_seller.addSellerFailed,
+                payload: error.response.data.errors.Description[0]
+            });
+        }
+
+        deleteMessage();
+    }
 
     return (
         <SellerContext.Provider
@@ -25,6 +61,8 @@ export const SellerState = ( props ) => {
                 searchModeStatus: state.searchModeStatus,
                 statusEditModeSeller: state.statusEditModeSeller,
                 infSellerEdit: state.infSellerEdit,
+                createSeller,
+                deleteMessage,
             }}
         >
             { props.children }

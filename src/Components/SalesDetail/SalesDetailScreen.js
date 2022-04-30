68ae6a26e804174f190c8b94394da728806d3eac
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-29 22:41:40 -0500

    Activamos las alertas en SalesDetail

--- src/Components/SalesDetail/SalesDetailScreen.js
+++ src/Components/SalesDetail/SalesDetailScreen.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react'
+import { AlertContext } from '../../Context/Alert/AlertContext';
 import { SalesDetailContext } from '../../Context/SalesDetail/SalesDetailContext';
 import { FloatingButtonClose } from '../UI/FloatingButton/FloatingButtonClose';
 import { HeadBoard } from '../UI/HeadBoard/HeadBoard';
@@ -11,11 +12,24 @@ export const SalesDetailScreen = () => {
     const { salesDetailList, message, typeMessage, listSalesDetailFound, searchModeStatus,
                 getSalesDetail, desactiveModeEdit, searchSalesDetail } = salesDetailContext;
 
+    const alertContext = useContext( AlertContext );
+    const { showAlert } = alertContext;
+
     const [ formValues, setFormValues ] = React.useState({ searchSalesDetailValue: ''});
     const { searchSalesDetailValue } = formValues;
     const handleInputChange = () => {}
     const handleResetSearchInput = () => {}
-
+    
+    /*
+        * Mostramos el mesaje si existe uno en el state
+        * El otro caso es que no se muestre ningun mensaje
+    */
+    useEffect( () => {
+        if( message ) {
+            showAlert( message, typeMessage );
+        }
+        // eslint-disable-next-line
+    } , [message] );
     
     useEffect(() => {
         getSalesDetail();

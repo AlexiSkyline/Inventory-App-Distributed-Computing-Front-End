0bc5249334b90b0735aa03cec4b1559f6d4aa1e8
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-29 22:39:55 -0500

    Activamos la acción de eliminar un salesDetail de la DB

--- src/Components/SalesDetail/TableSalesDetail.js
+++ src/Components/SalesDetail/TableSalesDetail.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { SalesDetailContext } from '../../Context/SalesDetail/SalesDetailContext';
+import { useActions } from '../../Hooks/useActions';
 
 export const TableSalesDetail = ({ titles, listSalesDetail, handleResetSearchInput }) => {
-    const handleUpdate = ( id ) => {}
-    const handleConfirm = ( id ) => {}
+    const salesDetailContext = useContext( SalesDetailContext );
+    const { deleteSalesDetail, activeModeEdit, disactiveSalesDetailSearchMode } = salesDetailContext;
 
+    /*
+        * Funcion para eliminar un detalle Ventas una vez que se confirma
+        * Recibe el id del detalle Ventas a eliminar
+        * Luego elimina el detalle Ventas
+        * Luego desactiva el modo de busqueda si esta activo
+        * Luego reinicia el input de busqueda de detalle Ventas
+    */
+    const handleDelete = ( id ) => {
+        deleteSalesDetail( id );
+        handleResetSearchInput();
+        disactiveSalesDetailSearchMode();
+    }
+
+    const [ handleConfirm, handleUpdate ] = useActions( 'El detalle de la venta', handleDelete, activeModeEdit );
+    
     return (
         <div className='table__container table__median'>  
             <table className='table'> 
